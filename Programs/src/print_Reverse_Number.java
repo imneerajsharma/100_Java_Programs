a9c117e294d9b019f0b72e88740b8886b23d7f23
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-16 14:10:31 -0500

    print_Reverse_Number

---
+++ Programs/src/print_Reverse_Number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class print_Reverse_Number {
+    static void reverse(int n){
+        int reverse=0;
+
+        while(n>0){
+            reverse=reverse*10;
+            reverse+=n%10;
+            n=n/10;
+        }
+
+        System.out.println("Reversed number is : "+reverse);
+    }
+    public static void main(String [] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter  a Value : ");
+        int input=scanner.nextInt();
+        reverse(input);
+
+    }
+}
