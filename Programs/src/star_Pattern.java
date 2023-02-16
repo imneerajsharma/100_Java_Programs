04b3268f2ed6b4e7fc673d2c388c32cdb632e1ee
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-16 13:00:52 -0500

    star pattern 6

--- Programs/src/star_Pattern.java
+++ Programs/src/star_Pattern.java
@@ -1,16 +1,8 @@
 import java.util.Scanner;
 public class star_Pattern {
-/*
-## Printing the pattern 1 ##
- *
- **
- ***
- ****
-
- */
-
     public static void pattern1(int n)
-    {
+    {   System.out.println();
+        System.out.println("Pattern  : 1 ");
         for(int i=0;i<=n;i++){
             for(int j=0 ; j<i;j++){
                 System.out.print("*");
@@ -20,18 +12,9 @@ public class star_Pattern {
         }
     }
 
-    /*
-## Printing the pattern 2 ##
-******
-******
-******
-******
-******
-******
-
- */
     public static void pattern2(int n)
-    {
+    {   System.out.println();
+        System.out.println("Pattern  : 2 ");
         for(int i=0;i<=n;i++){
             for(int j=0 ; j<=n;j++){
                 System.out.print("*");
@@ -41,19 +24,9 @@ public class star_Pattern {
         }
     }
 
-
-    /*
-## Printing the pattern 3 ##
-******
-*****
-****
-***
-**
-*
-
-*/
     public static void pattern3(int n)
-    {
+    {   System.out.println();
+        System.out.println("Pattern  : 3 ");
         for(int i=n;i>=0;i--){
             for(int j=0 ; j<=i;j++){
                 System.out.print("*");
@@ -63,18 +36,9 @@ public class star_Pattern {
         }
     }
 
-/*
-## Printing the pattern 4 ##
-     *
-    **
-   ***
-  ****
- *****
-******
-
-*/
 public static void pattern4(int n)
-{
+{   System.out.println();
+    System.out.println("Pattern  : 4 ");
     for(int i=0;i<=n;i++){
         for(int j=n ; j>i;j--){
             System.out.print(" ");
@@ -88,16 +52,52 @@ public static void pattern4(int n)
 
     }
 }
+    public static void pattern5(int n)
+    {   System.out.println();
+        System.out.println("Pattern  : 5 ");
+        for(int i=0;i<=n;i++){
+
+            for(int j=n ; j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=0 ; k<(i*2)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+    public static void pattern6(int n)
+    {
+        System.out.println();
+        System.out.println("Pattern  : 6 ");
+        for(int i=n;i>=0;i--){
+
+            for(int k=1 ; k<=(i*2)-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            for(int j=n ; j>=i;j--){
+                System.out.print(" ");
+            }
 
 
+        }
 
+    }
 
 
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Enter the Value : ");
-        int n=scanner.nextInt();
+//        System.out.print("Enter the Value : ");
+//        int n=scanner.nextInt();
+        int n=5;
+        pattern1(n);
+        pattern2(n);
+        pattern3(n);
         pattern4(n);
+        pattern5(n);
+        pattern6(n);
 
     }
 }
