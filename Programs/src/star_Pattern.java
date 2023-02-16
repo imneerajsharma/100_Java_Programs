cb1736becbbfd32064305ad339aaec22619efa77
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-16 12:31:59 -0500

    star pattern 3

--- Programs/src/star_Pattern.java
+++ Programs/src/star_Pattern.java
@@ -42,11 +42,33 @@ public class star_Pattern {
     }
 
 
+    /*
+## Printing the pattern 3 ##
+******
+*****
+****
+***
+**
+*
+
+*/
+    public static void pattern3(int n)
+    {
+        for(int i=n;i>=0;i--){
+            for(int j=0 ; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+    }
+
+
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter the Value : ");
         int n=scanner.nextInt();
-        pattern2(n);
+        pattern3(n);
 
     }
 }
