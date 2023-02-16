fc34e93a27e77228173012f69013761324716836
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-16 12:26:55 -0500

    star Pattern 2

--- Programs/src/star_Pattern.java
+++ Programs/src/star_Pattern.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 public class star_Pattern {
+/*
+## Printing the pattern 1 ##
+ *
+ **
+ ***
+ ****
 
-    public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
-        System.out.print("Enter the Value : ");
-        int n=scanner.nextInt();
+ */
 
+    public static void pattern1(int n)
+    {
         for(int i=0;i<=n;i++){
             for(int j=0 ; j<i;j++){
                 System.out.print("*");
@@ -13,5 +18,35 @@ public class star_Pattern {
             System.out.print("\n");
 
         }
+    }
+
+    /*
+## Printing the pattern 2 ##
+******
+******
+******
+******
+******
+******
+
+ */
+    public static void pattern2(int n)
+    {
+        for(int i=0;i<=n;i++){
+            for(int j=0 ; j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+    }
+
+
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the Value : ");
+        int n=scanner.nextInt();
+        pattern2(n);
+
     }
 }
