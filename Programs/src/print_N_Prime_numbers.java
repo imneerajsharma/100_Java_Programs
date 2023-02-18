78c3286b354a78a42358b439608b162e00a08b4d
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-17 20:54:31 -0500

    Optimized # print_N_Prime_numbers

--- Programs/src/print_N_Prime_numbers.java
+++ Programs/src/print_N_Prime_numbers.java
@@ -14,7 +14,7 @@ public class print_N_Prime_numbers {
     }
 
     static void print_N_Prime(int num){
-        int count=0;
+/*        int count=0;
         int n=0;
 
         while(count<num){
@@ -23,7 +23,31 @@ public class print_N_Prime_numbers {
                 count++;
             }
             n++;
+        }
+*/
 
+        // ######  Optimized code ######//
+        /*
+        1. Check if the number is even before calling the is_Prime() method:
+        Since all even numbers (except 2) are not prime, we can avoid calling the is_Prime() method for even numbers.
+
+        2. Start the loop from 3 and increment by 2:
+        Since all even numbers have already been handled, we can start the loop from 3 and
+        increment by 2 in order to only check odd numbers.
+
+        3. Use a for loop instead of a while loop: A for loop is better suited for this scenario
+        as it allows us to declare the loop counter variable and specify the condition in a single line.
+
+         */
+        if(num>=1){
+            System.out.println("2");
+        }
+        int counter=1;
+        for(int i=3;counter<num;i+=2){
+            if(is_Prime(i)){
+                System.out.println(i);
+                counter++;
+            }
         }
     }
 
@@ -32,12 +56,6 @@ public class print_N_Prime_numbers {
         System.out.print("Enter the number of Primes to Print : ");
         int num=scanner.nextInt();
         print_N_Prime(num);
-       // is_Prime(num);
-//        if(is_Prime(num)){
-//            System.out.println(num+" is a Prime Number");
-//        }
-//        else{
-//            System.out.println(num+" is not a Prime Number");
-//        }
+
     }
 }
