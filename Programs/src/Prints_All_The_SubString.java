1572ce935314c1a5b6c969ebf9e0dc6385f86212
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-17 23:02:52 -0500

    Optimized # print Sub strings
    The code provided is already optimized, but here are a few additional suggestions:
    
    Add a try-with-resources statement to close the Scanner object automatically when the input is complete.
    
    Rename the print_sub_String method to printSubstrings to follow the standard Java naming conventions for methods.
    
    Use System.lineSeparator() to add a newline instead of "\n" to make the code more platform-independent.
    
    ------
    
    which one is better approach :
    
    1: try-with-resources statement to close the Scanner object
    
    2: scanner.close()
    
    ------------
    
    Using a try-with-resources statement to close the Scanner object is generally considered to be a better approach than manually calling scanner.close().
    
    The main advantage of using a try-with-resources statement is that it ensures that the Scanner object is always closed, even if an exception is thrown. This is because the try-with-resources statement automatically calls the close() method on the Scanner object when the try block is exited, whether normally or due to an exception. This helps to prevent resource leaks and makes the code more robust.
    
    In contrast, manually calling scanner.close() can be error-prone, as it is possible to forget to call it, or to call it in the wrong place. If the close() method is not called, the resource (in this case, the Scanner object) will remain open and may cause issues in the program.
    
    So, in summary, using a try-with-resources statement to close the Scanner object is generally the better approach, as it ensures that the Scanner object is always properly closed and helps to prevent resource leaks.

--- Programs/src/Prints_All_The_SubString.java
+++ Programs/src/Prints_All_The_SubString.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 public class Prints_All_The_SubString {
 
-    static void print_sub_String(String str){
+    static void printSubstrings(String str){
 
-        System.out.println("All Sub Strings Are: ");
-        for(int i=0; i<=str.length();i++){
+        StringBuilder sb= new StringBuilder();
+
+        for(int i=0; i<str.length();i++){
             for(int j=1;j<=str.length()-i;j++){
-                System.out.print(str.substring(i,i+j)+" , ");
+                sb.append(str.substring(i,i+j)).append(" , ");
             }
-            System.out.println();
+            sb.append(System.lineSeparator());
 
         }
+
+        System.out.println(sb.toString());
     }
 
     public static void main (String[] args){
-        Scanner scanner =new Scanner(System.in);
-        System.out.print("Enter a String to print all its sub strings : ");
-        String str=scanner.nextLine();
+        try(Scanner scanner =new Scanner(System.in))
+        {
+            System.out.print("Enter a String to print all its sub strings : ");
+            String str=scanner.nextLine();
 
-        print_sub_String(str);
+            printSubstrings(str);
+        }
+
+
 
 
     }
