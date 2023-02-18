746c38f725a110b25e8b96d3c2e7507c823f9171
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-02-17 22:17:31 -0500

    Optimized Code #  Floyd's Triangle

--- Programs/src/floyds_Triangle.java
+++ Programs/src/floyds_Triangle.java
@@ -4,22 +4,46 @@ public class floyds_Triangle {
 
     static void print_floyd_Triangle(int num){
         int count=1;
+        StringBuilder sb= new StringBuilder();
         if(num<1){
             System.out.println("Pleas enter a number greater than 1");
         }
-        else if (num==1){
-            System.out.println("1");
-        }
+
+        /*
+            // #### Optimized Code #### //
+
+            1.  StringBuilder sb= new StringBuilder();
+            2.  sb.append(count).append(" ");
+            3.  sb.append("\n");
+            4.  sb.toString()
+
+            1. Creating a new instance of the StringBuilder class, which initializes an empty string buffer.
+
+            2.  Then, in the code I provided, the sb object is used to concatenate the output of the Floyd's Triangle numbers.
+                The append() method of StringBuilder is used to add each number to the string buffer.
+                By doing this, we are building the output string in memory rather than printing each number
+                to the console individually. This is generally more efficient because it reduces the number of I/O operations.
+
+            3.  Finally, the toString() method is called on the sb object to get the final string,
+                which is then printed to the console.
+
+            4.  In summary, StringBuilder is a class that provides a more efficient way to concatenate strings in Java.
+
+         */
         else{
             System.out.println("Floyd's Triangle :");
             for(int i=0;i<num;i++){
                 for(int j=0;j<=i;j++){
-                    System.out.print(count+" ");
+                    //System.out.print(count+" ");
+                    sb.append(count).append(" ");
                     count++;
                 }
-                System.out.println();
+                sb.append("\n");
+
+                //System.out.println();
             }
         }
+        System.out.print(sb.toString());
     }
 
 
